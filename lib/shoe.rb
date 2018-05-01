9e3138c8e2489d2c518d538a5c4fa533619d33fd
William J Teskey <willjteskey@gmail.com>
2018-05-01 01:24:12 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,3 @@
 # Make your shoe class here!
 
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
-  end
-
-  def cobble
-    @condition = "new" # change condition from "old" and returns => "new"
-    
-    puts "Your shoe is as good as new!"
-  end
